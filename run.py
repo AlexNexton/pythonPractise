08435ac9ae6093841232a7a19e729a793bb9be8d
AlexNexton <cianoc13@gmail.com>
2021-02-04 12:21:29 +0000

    added careers page

--- run.py
+++ run.py
@@ -21,6 +21,10 @@ def about():
 def contact():
     return render_template("contact.html", page_title="Contact")
 
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
