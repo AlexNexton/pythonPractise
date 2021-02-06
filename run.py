f5337076957321b3b7531482880d84604b37b5a5
AlexNexton <cianoc13@gmail.com>
2021-02-06 07:59:00 +0000

    Deployment to heroku

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
-app.secret_key = os.eviron.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -16,7 +16,7 @@ def index():
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
-        data= json.load(json_data)
+        data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 @app.route("/about/<member_name>")
